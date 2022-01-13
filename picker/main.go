37725ae74ef170fa7b898f0e8bd42706dec37b72
plarun <arunpandipava@gmail.com>
2022-01-13 22:30:49 +0530

    update

--- picker/main.go
+++ picker/main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	pb "github.com/plarun/scheduler/picker/data"
+	"github.com/plarun/scheduler/picker/queue"
+	"google.golang.org/grpc"
+)
+
+const port = 5556
+
+var waitingQueue = queue.NewWaitingQueue()
+
 func main() {
-	//
+	// client service to communicate with event-server
+	go startClient()
+
+	// server service to communicate with controller
+	serve()
+}
+
+func startClient() {
+	conn, err := grpc.Dial("localhost:5555", grpc.WithInsecure)
+	if err != nil {
+		log.Fatalf("connection failed: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewNextJobsClient(conn)
+	picker := NewJobPicker(client)
+	for true {
+		time.Sleep(time.Second * 5)
+		picker.Pick()
+	}
+}
+
+func serve() {
+	// addr := fmt.Sprintf(":%d", port)
+	// // Server listens on tcp port
+	// listen, err := net.Listen("tcp", addr)
+	// if err != nil {
+	// 	log.Fatalf("failed to listen %v", err)
+	// }
+
+	// // grpc server can have arguments for unary and stream as server options
+	// grpcServer := grpc.NewServer()
+	// // register all servers here
+	// pb.RegisterNextJobsServer(grpcServer, )
+
+	// fmt.Printf("Scheduler grpc server is running at port: %d\n", port)
+	// if err := grpcServer.Serve(listen); err != nil {
+	// 	log.Fatalf("failed to start server %v", err)
+	// }
 }
---
+++ picker/queue/queue.go
@@ -0,0 +1,102 @@
+package queue
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Double ended node
+type WaitingJob struct {
+	job  interface{}
+	prev *WaitingJob
+	next *WaitingJob
+}
+
+// WaitingQueue represents list of jobs waiting for next run
+type WaitingQueue struct {
+	in   *WaitingJob
+	out  *WaitingJob
+	size uint32
+}
+
+// newWaitingJob returns a new WaitingJob node
+func (wque *WaitingQueue) newWaitingJob(data interface{}) *WaitingJob {
+	return &WaitingJob{
+		job:  data,
+		prev: nil,
+		next: nil,
+	}
+}
+
+func (wque *WaitingQueue) push(data interface{}) error {
+	var node *WaitingJob = wque.newWaitingJob(data)
+	if wque.size == 0 {
+		wque.in = node
+		wque.out = node
+	} else {
+		node.next = wque.in
+		wque.in.prev = node
+		wque.in = node
+	}
+	wque.size++
+	return nil
+}
+
+func (wque *WaitingQueue) pop() (interface{}, error) {
+	if wque.size == 0 {
+		return nil, fmt.Errorf("waiting queue is empty")
+	}
+	var node *WaitingJob = wque.out
+	wque.out = wque.out.prev
+	if wque.out != nil {
+		wque.out.next = nil
+	}
+	wque.size--
+	if wque.size == 0 {
+		wque.in, wque.out = nil, nil
+	}
+	return node, nil
+}
+
+func (wque *WaitingQueue) empty() bool {
+	return wque.size == 0
+}
+
+// ConcurrentWaitingQueue is a concurrency wrapper for WaitingQueue
+type ConcurrentWaitingQueue struct {
+	lock *sync.Mutex
+	wQue *WaitingQueue
+}
+
+func NewWaitingQueue() *ConcurrentWaitingQueue {
+	queue := &ConcurrentWaitingQueue{
+		lock: &sync.Mutex{},
+		wQue: &WaitingQueue{
+			in:   nil,
+			out:  nil,
+			size: 0,
+		},
+	}
+	return queue
+}
+
+func (cwque *ConcurrentWaitingQueue) Push(data interface{}) error {
+	cwque.lock.Lock()
+	err := cwque.wQue.push(data)
+	cwque.lock.Unlock()
+	return err
+}
+
+func (cwque *ConcurrentWaitingQueue) Pop() (interface{}, error) {
+	cwque.lock.Lock()
+	data, err := cwque.wQue.pop()
+	cwque.lock.Unlock()
+	return data, err
+}
+
+func (cwque *ConcurrentWaitingQueue) Size() uint32 {
+	cwque.lock.Lock()
+	len := cwque.wQue.size
+	cwque.lock.Unlock()
+	return len
+}
