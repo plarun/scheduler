86e712f26886e2f1ccc579c43ead5b64ae2d50da
plarun <arunpandipava@gmail.com>
2022-01-20 19:47:04 +0530

    condition and status proto

--- monitor/main.go
+++ monitor/main.go
@@ -1,15 +1,5 @@
 package main
 
-/*
-1. controller -> (exit code) -> monitor -> event-server (update the status) -> monitor -> picker
-2. locker to stop the job in ready state to running (pque.pop)
-3. locker to stop the job in queued state to ready (wque.pop)
-*/
-
-/*
-How locker works
-*/
-
 func main() {
 
 }
