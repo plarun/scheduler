74b68dbb5eba5371867a6dd7503a8e590796b495
plarun <arunpandipava@gmail.com>
2023-01-29 19:08:09 +0530

    aligned the flow of polling and staging

--- internal/allocator/service/poll.go
+++ internal/allocator/service/poll.go
@@ -15,17 +15,38 @@ func NewTaskPoller(cycle time.Duration) *TaskPoller {
 	return &TaskPoller{cycle}
 }
 
-func (t *TaskPoller) Poll() error {
+func (t *TaskPoller) Stage() error {
 
 	if err := db.StageTasks(int(t.cycle)); err != nil {
-		return fmt.Errorf("Poll: %v", err)
+		return fmt.Errorf("Stage: %v", err)
 	}
 
 	if err := db.SetStagedStatus(); err != nil {
-		return fmt.Errorf("Poll: %v", err)
+		return fmt.Errorf("Stage: %v", err)
 	}
 
 	if err := db.MigrateStage(); err != nil {
+		return fmt.Errorf("Stage: %v", err)
+	}
+
+	return nil
+}
+
+func (t *TaskPoller) Poll() error {
+
+	if err := db.SetFlagPreQueue(); err != nil {
+		return fmt.Errorf("Poll: %v", err)
+	}
+
+	if err := db.QueueTasks(); err != nil {
+		return fmt.Errorf("Poll: %v", err)
+	}
+
+	if err := db.SetStatusQueue(); err != nil {
+		return fmt.Errorf("Poll: %v", err)
+	}
+
+	if err := db.SetFlagPostQueue(); err != nil {
 		return fmt.Errorf("Poll: %v", err)
 	}
 
