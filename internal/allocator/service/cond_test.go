a774f6cb24e875197f4cd19ed2f13bd3c05c3084
plarun <arunpandipava@gmail.com>
2023-02-02 10:00:41 +0530

    new test case added

--- internal/allocator/service/cond_test.go
+++ internal/allocator/service/cond_test.go
@@ -28,6 +28,30 @@ func TestConditionCheck(t *testing.T) {
 		"good 1": {condition: "su(task1)", stat: stat{"task1": task.StateSuccess}, want: true},
 		"good 2": {condition: "su(task2)", stat: stat{"task2": task.StateSuccess}, want: true},
 		"good 3": {condition: "su(task3)", stat: stat{"task3": task.StateSuccess}, want: true},
+		"good 4": {
+			condition: "su(task1)&su(task2)&(fa(task3)|nr(task4))",
+			stat: stat{
+				"task1": task.StateSuccess, "task2": task.StateSuccess,
+				"task3": task.StateFailure, "task4": task.StateFailure,
+			}, want: true},
+		"good 5": {
+			condition: "su(task1)&su(task2)&su(task3)&su(task4)",
+			stat: stat{
+				"task1": task.StateSuccess, "task2": task.StateSuccess,
+				"task3": task.StateSuccess, "task4": task.StateSuccess,
+			}, want: true},
+		"good 6": {
+			condition: "su(task1)&su(task2)&su(task3)&su(task4)",
+			stat: stat{
+				"task1": task.StateSuccess, "task2": task.StateSuccess,
+				"task3": task.StateSuccess, "task4": task.StateFailure,
+			}, want: false},
+		"good 7": {
+			condition: "su(task1)&su(task2)&su(task3)&su(task4)",
+			stat: stat{
+				"task1": task.StateFailure, "task2": task.StateSuccess,
+				"task3": task.StateSuccess, "task4": task.StateSuccess,
+			}, want: false},
 	}
 
 	for name, tc := range tests {
