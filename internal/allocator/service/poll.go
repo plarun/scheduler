bbabc4a86db7547557a0e120691a0defdc6ae2b2
plarun <arunpandipava@gmail.com>
2023-02-05 08:41:10 +0530

    renamed the function calls

--- internal/allocator/service/poll.go
+++ internal/allocator/service/poll.go
@@ -15,38 +15,44 @@ func NewTaskPoller(cycle time.Duration) *TaskPoller {
 	return &TaskPoller{cycle}
 }
 
+// Stage performs staging the scheduled tasks
 func (t *TaskPoller) Stage() error {
-
-	if err := db.StageTasks(int(t.cycle)); err != nil {
+	// lock tasks for staging
+	if err := db.LockForStaging(int(t.cycle)); err != nil {
+		return fmt.Errorf("StageTasks: %w", err)
+	}
+	// push locked tasks into staging area
+	if err := db.StageTasks(); err != nil {
 		return fmt.Errorf("Stage: %v", err)
 	}
-
+	// set task status to "staged"
 	if err := db.SetStagedStatus(); err != nil {
 		return fmt.Errorf("Stage: %v", err)
 	}
-
-	if err := db.MigrateStage(); err != nil {
+	// set task flag as staged
+	if err := db.SetStagedFlag(); err != nil {
 		return fmt.Errorf("Stage: %v", err)
 	}
 
 	return nil
 }
 
+// Poll performs queuing the staged tasks into queue
 func (t *TaskPoller) Poll() error {
 
-	if err := db.SetFlagPreQueue(); err != nil {
+	if err := db.LockForEnqueue(); err != nil {
 		return fmt.Errorf("Poll: %v", err)
 	}
 
-	if err := db.QueueTasks(); err != nil {
+	if err := db.EnqueueTasks(); err != nil {
 		return fmt.Errorf("Poll: %v", err)
 	}
 
-	if err := db.SetStatusQueue(); err != nil {
+	if err := db.SetQueueStatus(); err != nil {
 		return fmt.Errorf("Poll: %v", err)
 	}
 
-	if err := db.SetFlagPostQueue(); err != nil {
+	if err := db.SetQueuedFlag(); err != nil {
 		return fmt.Errorf("Poll: %v", err)
 	}
 
