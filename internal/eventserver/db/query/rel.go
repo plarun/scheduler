7e0e28824a63fbc970275bf5c6c7a2a189b8e2b8
plarun <arunpandipava@gmail.com>
2023-03-19 20:01:06 +0530

    renamed GetDependentTasks to GetDependentTasksStatus

--- internal/eventserver/db/query/rel.go
+++ internal/eventserver/db/query/rel.go
@@ -4,30 +4,37 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/plarun/scheduler/api/types/entity/task"
 	mysql "github.com/plarun/scheduler/internal/eventserver/db"
 )
 
-func GetDependentTasks(id int64) ([]int64, error) {
+func GetDependentTasksStatus(id int64) ([]*task.TaskStatus, error) {
 	db := mysql.GetDatabase()
 
-	qry := `Select task_id
-		From sched_task_relation
-		Where cond_task_id=?`
+	qry := `Select t.id, t.name, t.current_status
+		From sched_task t, sched_task_relation r
+		Where t.id=r.task_id And r.cond_task_id=?`
 
-	res := make([]int64, 0)
+	res := make([]*task.TaskStatus, 0)
 
 	rows, err := db.Query(qry, id)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return res, nil
 		}
-		return res, fmt.Errorf("GetDependentTasks: %w", err)
+		return res, fmt.Errorf("GetDependentTasksStatus: %w", err)
 	}
 
 	for rows.Next() {
 		var taskId int64
+		var name, status string
+
 		rows.Scan(&taskId)
-		res = append(res, taskId)
+		rows.Scan(&name)
+		rows.Scan(&status)
+
+		ts := task.NewTaskStatus(taskId, name, task.State(status))
+		res = append(res, ts)
 	}
 	return res, nil
 }
