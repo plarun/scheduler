ac0469eb6eb1e497a434c57999ff1a609a0fc556
plarun <arunpandipava@gmail.com>
2023-03-23 18:45:45 +0530

    added tcp listener for allocator

--- internal/allocator/server.go
+++ internal/allocator/server.go
@@ -1,22 +1,48 @@
 package allocator
 
 import (
+	"fmt"
 	"log"
+	"net"
 
 	db "github.com/plarun/scheduler/internal/allocator/db/mysql"
+	"github.com/plarun/scheduler/internal/allocator/handler"
 	"github.com/plarun/scheduler/internal/allocator/service"
+	"github.com/plarun/scheduler/proto"
+	"google.golang.org/grpc"
 )
 
 func Serve(port int) {
-	log.Println("Allocator starting...")
+	addr := fmt.Sprintf(":%d", port)
+
+	listen, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to create tcp listener on %s", addr)
+	}
+
+	log.Printf("tcp listening on %s", addr)
+
 	// connect to mysql db
 	if err := db.ConnectDB(); err != nil {
 		log.Fatal(err)
 	}
 
-	alloc := service.NewAllocator()
+	go func() {
+		log.Println("Allocator starting...")
 
-	if err := alloc.Start(); err != nil {
-		panic(err)
+		alloc := service.NewAllocator()
+
+		if err := alloc.Start(); err != nil {
+			panic(err)
+		}
+	}()
+
+	ser := grpc.NewServer()
+
+	// register all grpc services here
+	proto.RegisterWaitTaskServiceServer(ser, handler.NewTaskExecService())
+
+	if err := ser.Serve(listen); err != nil {
+		log.Fatal("failed to listen")
 	}
 }
