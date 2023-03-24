7b04145e51c025dd073b6bfe5176f8b2e375f46a
plarun <arunpandipava@gmail.com>
2023-03-24 20:06:50 +0530

    task definition service

--- proto/entity_grpc.pb.go
+++ proto/entity_grpc.pb.go
@@ -194,3 +194,91 @@ var ValidatedActionService_ServiceDesc = grpc.ServiceDesc{
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "entity.proto",
 }
+
+// TaskServiceClient is the client API for TaskService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type TaskServiceClient interface {
+	// GetDefinition gets the existing task's definition
+	GetDefinition(ctx context.Context, in *TaskDefinitionRequest, opts ...grpc.CallOption) (*TaskDefinitionResponse, error)
+}
+
+type taskServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewTaskServiceClient(cc grpc.ClientConnInterface) TaskServiceClient {
+	return &taskServiceClient{cc}
+}
+
+func (c *taskServiceClient) GetDefinition(ctx context.Context, in *TaskDefinitionRequest, opts ...grpc.CallOption) (*TaskDefinitionResponse, error) {
+	out := new(TaskDefinitionResponse)
+	err := c.cc.Invoke(ctx, "/proto.TaskService/GetDefinition", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// TaskServiceServer is the server API for TaskService service.
+// All implementations must embed UnimplementedTaskServiceServer
+// for forward compatibility
+type TaskServiceServer interface {
+	// GetDefinition gets the existing task's definition
+	GetDefinition(context.Context, *TaskDefinitionRequest) (*TaskDefinitionResponse, error)
+	mustEmbedUnimplementedTaskServiceServer()
+}
+
+// UnimplementedTaskServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedTaskServiceServer struct {
+}
+
+func (UnimplementedTaskServiceServer) GetDefinition(context.Context, *TaskDefinitionRequest) (*TaskDefinitionResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetDefinition not implemented")
+}
+func (UnimplementedTaskServiceServer) mustEmbedUnimplementedTaskServiceServer() {}
+
+// UnsafeTaskServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to TaskServiceServer will
+// result in compilation errors.
+type UnsafeTaskServiceServer interface {
+	mustEmbedUnimplementedTaskServiceServer()
+}
+
+func RegisterTaskServiceServer(s grpc.ServiceRegistrar, srv TaskServiceServer) {
+	s.RegisterService(&TaskService_ServiceDesc, srv)
+}
+
+func _TaskService_GetDefinition_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(TaskDefinitionRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TaskServiceServer).GetDefinition(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.TaskService/GetDefinition",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TaskServiceServer).GetDefinition(ctx, req.(*TaskDefinitionRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// TaskService_ServiceDesc is the grpc.ServiceDesc for TaskService service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var TaskService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "proto.TaskService",
+	HandlerType: (*TaskServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GetDefinition",
+			Handler:    _TaskService_GetDefinition_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "entity.proto",
+}
