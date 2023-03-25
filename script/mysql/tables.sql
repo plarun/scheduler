7961f041ab780caa2daee772ade93da97a6d529d
plarun <arunpandipava@gmail.com>
2023-03-25 12:25:54 +0530

    latest table queries

--- script/mysql/tables.sql
+++ script/mysql/tables.sql
@@ -8,95 +8,116 @@ Create Database If Not Exists sched_test;
 Use sched_test;
 
 -- sched_job contains tasks
-Create Table `sched_job` (
-    `job_id`          int Not Null AUTO_INCREMENT,
-    `parent_id`       int Default Null,
-    `job_name`        varchar(64) Not Null,
-    `job_type`        varchar(16) Not Null,
-    `owner_id`        int Default Null,
-    `machine`         int Default Null,
-    `run_flag`        varchar(16) Default Null,
-    `start_condition` varchar(2048) Default Null,
-    `command`         varchar(256) Default Null,
-    `std_out_log`     varchar(256) Default Null,
-    `std_err_log`     varchar(256) Default Null,
-    `label`           varchar(256) Default Null,
-    `job_profile`     varchar(256) Default Null,
-    `run_days_bit`    int Default Null,
-    `start_window`    time Default Null,
-    `end_window`      time Default Null,
-    `next_run`        time Default Null,
-    `priority`        int Default '0',
-    `created_on`      datetime Default CURRENT_TIMESTAMP,
-    `created_by`      varchar(32) Default Null,
-    `last_updated_on` datetime Default Null,
-    `last_updated_by` varchar(32) Default Null,
-    `last_start_time` datetime Default Null,
-    `last_end_time`   datetime Default Null,
-    `current_status`  varchar(16) Default 'IDLE',
+CREATE TABLE `sched_task` (
+   `id` int NOT NULL AUTO_INCREMENT,
+   `parent_id` int DEFAULT NULL,
+   `name` varchar(64) NOT NULL,
+   `type` varchar(16) NOT NULL,
+   `owner_id` int DEFAULT NULL,
+   `machine` int DEFAULT NULL,
+   `run_flag` varchar(16) DEFAULT NULL,
+   `start_condition` varchar(2048) DEFAULT NULL,
+   `command` varchar(256) DEFAULT NULL,
+   `std_out_log` varchar(256) DEFAULT NULL,
+   `std_err_log` varchar(256) DEFAULT NULL,
+   `label` varchar(256) DEFAULT NULL,
+   `profile` varchar(256) DEFAULT NULL,
+   `run_days_bit` int DEFAULT NULL,
+   `start_window` time DEFAULT NULL,
+   `end_window` time DEFAULT NULL,
+   `next_run` time DEFAULT NULL,
+   `priority` int DEFAULT '0',
+   `created_on` datetime DEFAULT CURRENT_TIMESTAMP,
+   `created_by` varchar(32) DEFAULT NULL,
+   `last_updated_on` datetime DEFAULT NULL,
+   `last_updated_by` varchar(32) DEFAULT NULL,
+   `last_start_time` datetime DEFAULT NULL,
+   `last_end_time` datetime DEFAULT NULL,
+   `current_status` varchar(16) DEFAULT 'IDLE',
+   `lock_flag` int DEFAULT '0',
 
-    Primary Key (`job_id`),
-    Key         `job_name` (`job_name`)
-);
+   PRIMARY KEY (`id`),
+   KEY `job_name` (`name`)
+ ) ;
 
 -- batch run details
-Create Table `sched_batch_run` (
-    `job_id`     int Not Null,
-    `start_time` time Default Null
+CREATE TABLE `sched_batch_run` (
+   `task_id` int NOT NULL,
+   `start_time` time DEFAULT NULL,
+
+   KEY `task_id` (`task_id`),
+   CONSTRAINT `sched_batch_run_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `sched_task` (`id`)
 );
 
  -- window run details
-Create Table `sched_window_run` (
-    `job_id`    int Not Null,
-    `start_min` int Default Null
+CREATE TABLE `sched_window_run` (
+   `task_id` int NOT NULL,
+   `start_min` int DEFAULT NULL,
+
+   KEY `task_id` (`task_id`),
+   CONSTRAINT `sched_window_run_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `sched_task` (`id`)
 );
 
 -- start condition relation between tasks
-Create Table `sched_job_relation` (
-    `job_id`      int Not Null,
-    `cond_job_id` int Not Null
+CREATE TABLE `sched_task_relation` (
+   `task_id` int NOT NULL,
+   `cond_task_id` int NOT NULL,
+
+   KEY `task_id_idx` (`task_id`),
+   KEY `cond_task_id_idx` (`cond_task_id`)
 );
 
+-- queued tasks
+CREATE TABLE `sched_queue` (
+   `task_id` int NOT NULL,
+   `sys_entry_date` datetime DEFAULT CURRENT_TIMESTAMP,
+   `priority` int DEFAULT NULL,
+   `lock_flag` int NOT NULL DEFAULT '0',
 
-Create Table `sched_abort` (
-    `job_id`         int Not Null,
-    `sys_entry_date` datetime Default CURRENT_TIMESTAMP,
-    `lock_flag`      int Default Null
+   PRIMARY KEY (`task_id`)
 );
 
-Create Table `sched_exec` (
-    `job_id`        int Not Null,
-    `last_beat`     datetime Default Null,
-    `remote_server` varchar(256) Default Null,
-    `remote_fid`    varchar(256) Default Null,
-    `lock_flag`     int Default Null
+-- tasks ready for execution
+CREATE TABLE `sched_ready` (
+   `task_id` int NOT NULL,
+   `sys_entry_date` datetime DEFAULT CURRENT_TIMESTAMP,
+   `priority` int DEFAULT NULL,
+   `lock_flag` int DEFAULT '0',
+
+   PRIMARY KEY (`task_id`)
 );
 
-Create Table `sched_queue` (
-    `job_id`         int Not Null,
-    `sys_entry_date` datetime Default CURRENT_TIMESTAMP,
-    `priority`       int Default Null,
-    `lock_flag`      int Default Null
-)
+-- tasks tracked through execution cycle
+CREATE TABLE `sched_stage` (
+   `task_id` int NOT NULL,
+   `sys_entry_date` datetime DEFAULT CURRENT_TIMESTAMP,
+   `priority` int NOT NULL DEFAULT '0',
+   `flag` int NOT NULL,
+   `is_bundle` int DEFAULT NULL,
+   `lock_flag` int DEFAULT NULL,
 
-Create Table `sched_ready` (
-   `job_id`         int Not Null,
-   `sys_entry_date` datetime Default CURRENT_TIMESTAMP,
-   `priority`       int Default Null,
-   `lock_flag`      int Default Null
+   PRIMARY KEY (`task_id`)
 );
 
-Create Table `sched_stage` (
-    `job_id`         int Not Null,
-    `sys_entry_date` datetime DEFAULT CURRENT_TIMESTAMP,
-    `priority`       int Not Null DEFAULT '0',
-    `flag`           int Not Null,
-    `is_bundle`      int Default Null,
-    `lock_flag`      int Default Null
+-- tasks waiting for its starting condition to satisfy
+CREATE TABLE `sched_wait` (
+   `task_id` int NOT NULL,
+   `sys_entry_date` datetime DEFAULT CURRENT_TIMESTAMP,
+   `priority` int DEFAULT NULL,
+
+   PRIMARY KEY (`task_id`)
 );
 
-Create Table `sched_wait` (
-    `job_id`         int Not Null,
-    `sys_entry_date` datetime Default CURRENT_TIMESTAMP,
-    `priority`       int Default Null
+-- tasks' run history
+CREATE TABLE `sched_run_history` (
+   `run_id` int NOT NULL AUTO_INCREMENT,
+   `task_id` int NOT NULL,
+   `seq_id` int NOT NULL,
+   `start_time` datetime DEFAULT NULL,
+   `end_time` datetime DEFAULT NULL,
+   `status` varchar(16) DEFAULT NULL,
+   
+   PRIMARY KEY (`run_id`),
+   KEY `task_id` (`task_id`,`seq_id`),
+   CONSTRAINT `sched_run_history_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `sched_task` (`id`)
 );
