f077768d93d407121368e262aa0b414a76fe3162
plarun <arunpandipava@gmail.com>
2023-03-30 17:40:21 +0530

    added handler for send event

--- internal/eventserver/handler/task.go
+++ internal/eventserver/handler/task.go
@@ -4,8 +4,11 @@ import (
 	"errors"
 
 	es "github.com/plarun/scheduler/internal/eventserver/service"
+	er "github.com/plarun/scheduler/pkg/error"
 	"github.com/plarun/scheduler/proto"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // TaskExecService is a grpc server for communicating with worker service
@@ -24,11 +27,12 @@ func (s TaskService) GetDefinition(ctx context.Context, req *proto.TaskDefinitio
 	}
 
 	// get task definition
-	if tasks, err := es.GetTaskDefinition(ctx, req.TaskName); err != nil {
-		for errors.Unwrap(err) != nil {
-			err = errors.Unwrap(err)
+	tasks, err := es.GetTaskDefinition(ctx, req.TaskName)
+	if err != nil {
+		if errors.Is(err, er.DatabaseError{}) {
+			return res, status.Errorf(codes.Internal, "Internal Server error")
 		}
-		return res, err
+		return res, status.Errorf(codes.Unknown, "Unknown error")
 	} else {
 		res.IsValid = true
 		res.Task = tasks
@@ -43,11 +47,12 @@ func (s TaskService) GetStatus(ctx context.Context, req *proto.TaskLatestStatusR
 	}
 
 	// get task latest status
-	if st, err := es.GetTaskLatestStatus(ctx, req.TaskName); err != nil {
-		for errors.Unwrap(err) != nil {
-			err = errors.Unwrap(err)
+	st, err := es.GetTaskLatestStatus(ctx, req.TaskName)
+	if err != nil {
+		if errors.Is(err, er.DatabaseError{}) {
+			return res, status.Errorf(codes.Internal, "Internal Server error")
 		}
-		return res, err
+		return res, status.Errorf(codes.Unknown, "Unknown error")
 	} else {
 		res.IsValid = true
 		res.Status = st
@@ -62,14 +67,37 @@ func (s TaskService) GetRuns(ctx context.Context, req *proto.TaskRunsRequest) (*
 	}
 
 	// get task latest status
-	if runs, err := es.GetTaskRuns(ctx, req.TaskName, req.Count, req.RunDate); err != nil {
-		for errors.Unwrap(err) != nil {
-			err = errors.Unwrap(err)
+	runs, err := es.GetTaskRuns(ctx, req.TaskName, req.Count, req.RunDate)
+	if err != nil {
+		if errors.Is(err, er.DatabaseError{}) {
+			return res, status.Errorf(codes.Internal, "Internal Server error")
 		}
-		return res, err
+		return res, status.Errorf(codes.Unknown, "Unknown error")
 	} else {
 		res.IsValid = true
 		res.Runs = runs
+		return res, nil
+	}
+}
+
+func (s TaskService) SendEvent(ctx context.Context, req *proto.TaskEventRequest) (*proto.TaskEventResponse, error) {
+	res := &proto.TaskEventResponse{}
+
+	// get task latest status
+	r, err := es.ActionTaskEvent(ctx, req.TaskName, req.Event)
+	if err != nil {
+		if errors.Is(err, er.DatabaseError{}) {
+			return res, status.Errorf(codes.Internal, "Internal Server error")
+		}
+		return res, status.Errorf(codes.Unknown, "Unknown error")
+	}
+	if !r.IsSuccess {
+		res.Success = false
+		res.Msg = r.Msg.Error()
+		return res, nil
+	} else {
+		res.Success = true
+		res.Msg = ""
 		return res, nil
 	}
 }
