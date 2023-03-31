09764b5d532485888c73d597e3850965665b0920
plarun <arunpandipava@gmail.com>
2023-03-31 11:04:19 +0530

    added event command script

---
+++ script/cmd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+alias schd_def="./schd_def.sh"; export schd_def
+alias schd_event="./schd_event.sh"; export schd_event
+alias schd_runs="./schd_runs.sh"; export schd_runs
+alias schd_status="./schd_status.sh"; export schd_status
+alias schd_task="./schd_task.sh"; export schd_task
